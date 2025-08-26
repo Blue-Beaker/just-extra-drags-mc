ecebdf58e210129bb7f613ec5a2f7accbb109a46
blue-beaker <yyy5566778899@126.com>
2025-08-27 00:55:46 +0800

    Fix BC compat

--- src/main/java/io/bluebeaker/justextradrags/compat/BCCompat.java
+++ src/main/java/io/bluebeaker/justextradrags/compat/BCCompat.java
@@ -12,20 +12,33 @@ import buildcraft.transport.gui.GuiEmzuliPipe_BC8;
 import io.bluebeaker.justextradrags.JustExtraDragsConfig;
 import io.bluebeaker.justextradrags.config.ConfigEntry;
 import io.bluebeaker.justextradrags.config.JXDConfigManager;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 
 public class BCCompat {
     public static void addBuildcraftCompat(){
+        Class guiDiamondPipe = null;
+        Class guiDiamondWoodPipe = null;
+        Class guiEmzuliPipe = null;
+        Class guiGate = null;
         if (JustExtraDragsConfig.BCTransport&& Loader.isModLoaded("buildcrafttransport"))
         {
-            JXDConfigManager.addEntry(new ConfigEntry(GuiDiamondPipe.class, ContainerDiamondPipe.class,SlotPhantom.class,true));
+            if(FMLCommonHandler.instance().getSide().isClient()){
+                guiDiamondPipe= GuiDiamondPipe.class;
+                guiDiamondWoodPipe= GuiDiamondWoodPipe.class;
+                guiEmzuliPipe= GuiEmzuliPipe_BC8.class;
+            }
+            JXDConfigManager.addEntry(new ConfigEntry(guiDiamondPipe, ContainerDiamondPipe.class,SlotPhantom.class,false));
 
-            JXDConfigManager.addEntry(new ConfigEntry(GuiDiamondWoodPipe.class, ContainerDiamondWoodPipe.class,SlotPhantom.class,true));
+            JXDConfigManager.addEntry(new ConfigEntry(guiDiamondWoodPipe, ContainerDiamondWoodPipe.class,SlotPhantom.class,false));
 
-            JXDConfigManager.addEntry(new ConfigEntry(GuiEmzuliPipe_BC8.class, ContainerEmzuliPipe_BC8.class,SlotPhantom.class,true));
+            JXDConfigManager.addEntry(new ConfigEntry(guiEmzuliPipe, ContainerEmzuliPipe_BC8.class,SlotPhantom.class,false));
         }
         if (JustExtraDragsConfig.BCSilicon&&Loader.isModLoaded("buildcraftsilicon")) {
-            JXDConfigManager.addEntry(new ConfigEntry(GuiGate.class, ContainerGate.class,SlotPhantom.class,true));
+            if(FMLCommonHandler.instance().getSide().isClient()){
+                guiGate= GuiGate.class;
+            }
+            JXDConfigManager.addEntry(new ConfigEntry(guiGate, ContainerGate.class,SlotPhantom.class,false));
         }
     }
 }
